6f444022fee6fec3bf08042732d670cac614246d
guilbertf <guilbertf@a11p7.fil.univ-lille1.fr>
2015-11-06 16:10:50 +0100

    letter test ? pas taper

--- courriers/tests/tests/letter/LetterTest.java
+++ courriers/tests/tests/letter/LetterTest.java
@@ -1,5 +1,19 @@
 package tests.letter;
 
-public class LetterTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+import courriers.letter.Letter;
+
+public abstract class LetterTest {
+
+	abstract Letter<?> createLetter();
+	
+	@Test
+	public void costInLetterTest(){
+		Letter<?> letter = createLetter();
+		assertTrue(letter.getCost()>0);
+	}
+	
 }
